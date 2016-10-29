fefd6953665c25e0f23d398f113c0a3c52770554
Jeseekia Vaughn <jeseekia@gmail.com>
2016-10-29 19:47:22 -0400

    Creating preloader

---
+++ Preloader.js
@@ -0,0 +1,16 @@
+RabbitDefender.Preloader = function(game){
+
+  this.preloadbar = null;
+  this.titletext = null;
+  this.ready = false;
+
+};
+
+RabbitDefender.Preloader.prototype = {
+
+  preload: function(){},
+
+  create: function(){},
+
+  update: function(){}
+};
